08660a1c6c7664ada6ea48ed416bb9946de120fb
Jabjabrik <Yusufjabriko>
2023-03-04 21:27:33 +0700

    'commit'

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ import serverless from 'serverless-http'
 
 const app = express()
 
-app.get('/', (req, res) => res.json({ message: 'hello world cuyy' }))
-// app.listen(3000, () => console.log('server is running on http://localhost:3000'))
+app.get('/', (req, res) => res.json({ message: 'Hello world' }))
+
+if (process.env.NODE_ENV === 'dev') {
+    app.listen(3000, () => console.log('server is running on http://localhost:3000'))
+}
+
 export const handler = serverless(app)
